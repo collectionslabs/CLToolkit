a4e68d7e0bdf5c2899d7a6300eb395b01140cffd
Tony Xiao <tony@collections.me>
2013-07-18 23:52:31 -0700

    Initial Commit

---
+++ CLToolkit/CLToolkit.h
@@ -0,0 +1,13 @@
+//
+//  CLToolkit.h
+//  CLToolkit
+//
+//  Created by Tony Xiao on 7/18/13.
+//  Copyright (c) 2013 Collections Labs, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLToolkit : NSObject
+
+@end
---
+++ CLToolkit/CLToolkit.m
@@ -0,0 +1,13 @@
+//
+//  CLToolkit.m
+//  CLToolkit
+//
+//  Created by Tony Xiao on 7/18/13.
+//  Copyright (c) 2013 Collections Labs, Inc. All rights reserved.
+//
+
+#import "CLToolkit.h"
+
+@implementation CLToolkit
+
+@end
