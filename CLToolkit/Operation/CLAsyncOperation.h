243ceead327aa52f0d20312542a17dec34bb570f
Tony Xiao <tony@happybits.co>
2013-10-17 00:08:27 -0700

    Adding CLOperation, modeled after AFURLConnectionOperation, provide state management, pause, resume, progress, success, failure and dependency checking

--- CLToolkit/Operation/CLAsyncOperation.h
+++ CLToolkit/Operation/CLAsyncOperation.h
@@ -48,3 +48,51 @@
 - (void)setCompletionBlockWithOperation:(void (^)(NSOperation *operation))block;
 
 @end
+
+typedef NS_ENUM(NSInteger, CLOperationState) {
+    CLOperationStateNotStarted = 0,
+    CLOperationStateExecuting,
+    CLOperationStatePaused,
+    CLOperationStateCancelled,
+    CLOperationStateSucceeded,
+    CLOperationStateFailed,
+};
+
+@interface CLOperation : NSOperation
+
+@property (assign, readonly) CLOperationState state;
+@property (assign, readonly) CGFloat progress;
+@property (strong, readonly) id result;
+@property (strong, readonly) NSError *error;
+
+// Sends NSNumber (progress) for next, complete when finish and error when fail
+@property (readonly) RACSignal *progressSignal;
+// Sends id (result) for next, complete when finish and error when fail
+@property (readonly) RACSignal *resultSignal;
+
+// Called by user of operation
+
+- (void)start;
+- (void)cancel;
+- (void)pause;
+- (void)resume;
+
+// Called by sublcass of operation only
+
+- (void)updateProgress:(CGFloat)progress;
+
+- (void)succeedWithResult:(id)result;
+- (void)failWithError:(NSError *)error;
+- (void)finishWithCancellation;
+
+// No-op implementations that should be overriden by subclasses
+
+- (void)operationDidStart;
+- (void)operationDidPause;
+- (void)operationDidResume;
+- (void)operationDidCancel;
+- (void)operationDidFail;
+- (void)operationDidSucceed;
+- (void)operationDidFinish; // Finish = sum of cancel, fail, succeed
+
+@end
--- CLToolkit/Operation/CLAsyncOperation.m
+++ CLToolkit/Operation/CLAsyncOperation.m
@@ -129,3 +129,198 @@
 }
 
 @end
+
+
+@interface CLOperation ()
+
+@property (assign) BOOL isExecuting;
+@property (assign) BOOL isFinished;
+
+@property (assign, readwrite) CGFloat progress;
+@property (strong, readwrite) id result;
+@property (strong, readwrite) NSError *error;
+
+
+@end
+
+@implementation CLOperation {
+    RACSubject *_progressSignal;
+    RACSubject *_resultSignal;
+}
+
+- (id)init {
+    if (self = [super init]) {
+        _progressSignal = [RACReplaySubject replaySubjectWithCapacity:1];
+        _resultSignal = [RACReplaySubject replaySubjectWithCapacity:1];
+    }
+    return self;
+}
+
+- (BOOL)isNewStateValid:(CLOperationState)newState {
+    CLOperationState currentState = self.state;
+    switch (currentState) {
+        case CLOperationStateNotStarted:
+            switch (newState) {
+                case CLOperationStateExecuting:
+                case CLOperationStateCancelled:
+                    return YES;
+                default:
+                    return NO;
+            }
+        case CLOperationStateExecuting:
+            switch (newState) {
+                case CLOperationStateCancelled:
+                case CLOperationStatePaused:
+                case CLOperationStateSucceeded:
+                case CLOperationStateFailed:
+                    return YES;
+                default:
+                    return NO;
+            }
+        case CLOperationStatePaused:
+            switch (newState) {
+                case CLOperationStateExecuting:
+                case CLOperationStateCancelled:
+                    return YES;
+                default:
+                    return NO;
+            }
+        case CLOperationStateSucceeded:
+        case CLOperationStateFailed:
+        case CLOperationStateCancelled:
+            return NO;
+    }
+}
+
+- (BOOL)transitionToState:(CLOperationState)state {
+    @synchronized(self) {
+        if ([self isNewStateValid:state]) {
+            [self willChangeValueForKey:@keypath(self, state)];
+            _state = state;
+            [self didChangeValueForKey:@keypath(self, state)];
+            switch (state) {
+                case CLOperationStateExecuting:
+                    self.isExecuting = YES;
+                    break;
+                case CLOperationStatePaused:
+                    self.isExecuting = NO;
+                    break;
+                case CLOperationStateSucceeded:
+                case CLOperationStateFailed:
+                case CLOperationStateCancelled:
+                    self.isExecuting = NO;
+                    self.isFinished = YES;
+                    break;
+                default:
+                    break;
+            }
+            return YES;
+        }
+        return NO;
+    }
+}
+
+- (void)setDefaultErrorWithCode:(NSInteger)code description:(NSString *)description {
+    @synchronized(self) {
+        if (!self.error)
+            self.error = [NSError errorWithDomain:@"CLOperation"
+                                             code:code
+                                         userInfo:@{NSLocalizedDescriptionKey: description}];
+    }
+}
+
+- (BOOL)isConcurrent {
+    return YES;
+}
+
+// For use by sublcass
+
+- (void)updateProgress:(CGFloat)progress {
+    self.progress = progress;
+    [_progressSignal sendNext:@(progress)];
+}
+
+- (void)succeedWithResult:(id)result {
+    self.result = result;
+    if ([self transitionToState:CLOperationStateSucceeded]) {
+        [self operationDidSucceed];
+        [_progressSignal sendCompleted];
+        [_resultSignal sendNextAndComplete:result];
+    }
+}
+
+- (void)failWithError:(NSError *)error {
+    self.error = error;
+    if ([self transitionToState:CLOperationStateFailed]) {
+        [self operationDidFail];
+        [_progressSignal sendError:error];
+        [_resultSignal sendError:error];
+    }
+}
+
+- (void)finishWithCancellation {
+    if ([self transitionToState:CLOperationStateCancelled]) {
+        [self operationDidCancel];
+        [self setDefaultErrorWithCode:NSUserCancelledError
+                          description:$str(@"Operation cancelled %@", self)];
+        [_progressSignal sendError:self.error];
+        [_resultSignal sendError:self.error];
+    }
+}
+
+// User Facing API
+
+- (void)start {
+    if ([self transitionToState:CLOperationStateExecuting]) {
+        // Check dependencies.
+        for (NSOperation *operation in self.dependencies) {
+            if ([operation isKindOfClass:[CLOperation class]]) {
+                switch ([(CLOperation *)operation state]) {
+                    case CLOperationStateFailed:
+                        [self setDefaultErrorWithCode:-1
+                                          description:$str(@"Operation %@ dependency failed %@", self, operation)];
+                        [self failWithError:self.error];
+                        return;
+                    case CLOperationStateCancelled:
+                        [self setDefaultErrorWithCode:NSUserCancelledError
+                                          description:$str(@"Operation %@ dependency cancelled %@", self, operation)];
+                        [self cancel];
+                        [self finishWithCancellation];
+                        return;
+                    default:
+                        NSParameterAssert([(CLOperation *)operation state] == CLOperationStateSucceeded);
+                        break;
+                }
+            }
+        }
+        [self operationDidStart];
+    }
+}
+
+- (void)cancel {
+    [super cancel];
+    // NOTE: isCancelled will be changed immediately by super, but
+    // state doesn't get changed to CLOperationStateCancelled until finishCancellation is called
+}
+
+- (void)pause {
+    if ([self transitionToState:CLOperationStatePaused])
+        [self operationDidPause];
+}
+
+- (void)resume {
+    if ([self transitionToState:CLOperationStatePaused])
+        [self operationDidResume];
+}
+
+// Subclass Stubs
+
+- (void)operationDidStart { }
+- (void)operationDidPause { }
+- (void)operationDidResume { }
+- (void)operationDidCancel { }
+- (void)operationDidFail { }
+- (void)operationDidSucceed { }
+- (void)operationDidFinish { }
+
+@end
