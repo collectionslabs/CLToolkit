2afdaf0aeab5d1674e179855ef86bd5e97118973
Tony Xiao <tony@happybits.co>
2013-11-06 02:09:45 -0800

    Adding DEFINE_SHARED_INSTANCE_USING_BLOCK macro

--- CLToolkit/Core/Core.h
+++ CLToolkit/Core/Core.h
@@ -155,4 +155,12 @@ void LogImage(IMAGE_CLASS *image);
 #define $archive(obj)                    [NSKeyedArchiver archivedDataWithRootObject:obj]
 #define $unarchive(data)                 [NSKeyedUnarchiver unarchiveObjectWithData:data]
 
+#define DEFINE_SHARED_INSTANCE_USING_BLOCK(block) \
+    static dispatch_once_t pred; \
+    __strong static id _sharedObject = nil; \
+    dispatch_once(&pred, ^{ \
+        _sharedObject = block(); \
+    }); \
+    return _sharedObject; \
+
 #endif
